60b029d3fce310a5c17f82e0f9d1de43b35abbd2
Hailee Kenney <hailee@puppetlabs.com>
2015-06-16 16:22:31 -0700

    Add basic command line parsing

--- puppet_hack_data.rb
+++ puppet_hack_data.rb
@@ -1,7 +1,33 @@
 require 'octokit'
+require 'optparse'
 
 class PuppetHackData
 
-  PullRequest = Struct.new("PullRequest", :repo_owner, :repo_name, :pull_request_number, :merge_status, :author)
-  puts "hello world"
+  options = {}
+  repos = []
+
+
+  OptionParser.new do |opts|
+    opts.on("--oauth_token TOKEN") do |token|
+      options[:token] = token
+    end
+
+    opts.on("--start_date DATE") do |date|
+      options[:start_date] = date
+    end
+
+    opts.on("--end_date DATE") do |date|
+      options[:end_date] = date
+    end
+
+    opts.on("--closed") do
+      options[:closed] = true
+    end
+  end.parse!
+
+  ARGV.each do |arg|
+    repos << arg
+  end
+
+  puts repos
 end
